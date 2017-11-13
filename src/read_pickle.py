d0d951c0b45f915c50f8b9f5ae522dde92461a9e
amritbhanu <amritbhanu@gmail.com>
2017-11-13 14:25:05 -0500

    saved dataset2 into csv

--- src/read_pickle.py
+++ src/read_pickle.py
@@ -8,6 +8,7 @@ sys.dont_write_bytecode = True
 
 import pickle
 import numpy as np
+import pandas as pd
 
 with open("../dump/dataset1_untuned.pickle", 'rb') as handle:
     l = pickle.load(handle)
@@ -16,3 +17,9 @@ for i in l.keys():
     print(i)
     print(np.median(l[i][0]),np.percentile(l[i][0],75)-np.percentile(l[i][0],25))
     print(l[i][1])
+
+# data=np.load("../data/dataset2/mergedata.npy")
+# label=np.load("../data/dataset2/label.npy")
+# df=pd.DataFrame(data)
+# df['labels']=pd.DataFrame(label)
+# df.to_csv("../data/dataset2.csv",index=False)
