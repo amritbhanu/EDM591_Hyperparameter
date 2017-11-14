a6dc2232191d8b44de8dadd1d45a3a00237bfb19
aagrawa8 <aagrawa8@ncsu.edu>
2017-11-14 09:35:56 -0500

    save

---
+++ src/run.sh
@@ -0,0 +1,11 @@
+#! /bin/tcsh
+#rm out/*
+#rm err/*
+#rm log/*
+
+#datasets name
+#foreach VAR ("cs")
+#no_cross_tune_grow_oracle
+foreach VAR ("dataset2")
+  bsub -q standard -W 5000 -n 16 -o ./out/$VAR.out.%J -e ./err/$VAR.err.%J /share/tjmenzie/aagrawa8/miniconda2/bin/python2.7 main.py _test "$VAR" > log/"$VAR".log
+end
