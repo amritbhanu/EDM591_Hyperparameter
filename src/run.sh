98660018e792ff61a4230084048854d6f2e1c3f8
aagrawa8 <aagrawa8@ncsu.edu>
2017-11-14 16:14:28 -0500

    early termination

--- src/run.sh
+++ src/run.sh
@@ -6,6 +6,6 @@
 #datasets name
 #foreach VAR ("cs")
 #no_cross_tune_grow_oracle
-foreach VAR ("dataset2")
+foreach VAR ("dataset1")
   bsub -q standard -W 5000 -n 16 -o ./out/$VAR.out.%J -e ./err/$VAR.err.%J /share/tjmenzie/aagrawa8/miniconda2/bin/python2.7 main.py _test "$VAR" > log/"$VAR".log
 end
